b6e549db9475753d9834cbd43c76f4301bb42d8e
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-16 19:08:50 -0300

    Primeiro Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+             Console.WriteLine("Digite sua idade:");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Olá ,eu sou {0} e tenho {1} anos", nome, idade);
+    }
+    }
+}
